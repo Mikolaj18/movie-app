cc7cc51a0e5457b41303ac0ff3757a5c32bdf5e7
Mikołaj Urbański <m.urbanski@goldensubmarine.com>
2023-05-08 22:03:41 +0200

    Some fixes

--- client/src/Components/Filter.jsx
+++ client/src/Components/Filter.jsx
@@ -1,20 +1,24 @@
-export const Filter = ({category, filter, categories}) => {
+export const Filter = ({ category, filter, categories }) => {
+    const uniqueCategories = [...new Set(categories.map((cat) => cat.category))];
     return (
         <>
-            <div>Wyszukaj wg kategorii</div>
+            <h2>Wyszukaj wg kategorii</h2>
             <div>
                 <select
+                    className="form-select form-select-lg mb-3"
                     name="category-list"
                     id="category-list"
                     onChange={filter}
                     value={category}
                 >
                     <option value=""></option>
-                    {categories.map(cat => (
-                        <option key={cat.id} value={cat.category}>{cat.category}</option>
+                    {uniqueCategories.map((cat) => (
+                        <option key={cat} value={cat}>
+                            {cat}
+                        </option>
                     ))}
                 </select>
             </div>
         </>
-    )
-}
+    );
+};
--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useMemo, useState} from "react";
 import {moviesDataGetAll} from "../db/moviesDataGetAll";
 import SingleMovie from "../Components/SingleMovie";
 import {Filter} from "../Components/Filter";
+import {Link} from "react-router-dom";
 
 const Movies = () => {
     const [movies, setMovies] = useState([]);
@@ -25,22 +26,35 @@ const Movies = () => {
         return movies.filter((item) => item.category === selectedCategory);
     }
     const filteredList = useMemo(getFilteredList, [selectedCategory, movies]);
-
+    if(!movies) {
+        return (
+            <h1>Ładowanie...</h1>
+        )
+    } else {
     return (
         <div>
             <Filter category={selectedCategory} categories={movies} filter={handleCategoryChange}/>
-            <h1>Moja lista filmów</h1>
-            {filteredList.map(movie => (
-               <SingleMovie
-                   key={movie.id}
-                   id={movie.id}
-                   title={movie.title}
-                   category={movie.category}
-                   img={movie.img}
-                   short_desc={movie.short_desc}/>
-            ))}
+            <h1 className="text-primary">Moja lista filmów</h1>
+            <button type="button" className="btn btn-primary mb-3 mt-3"><Link className="text-white text-decoration-none" to={`/add`}>Dodaj kolejny film</Link></button>
+            <div className="py-5 mb-5 bg-light">
+                <div className="container">
+                    <div className="row">
+                        {filteredList.map(movie => (
+                            <SingleMovie
+                                key={movie.id}
+                                id={movie.id}
+                                title={movie.title}
+                                category={movie.category}
+                                img={movie.img}
+                                short_desc={movie.short_desc}/>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
+    }
 }
 
 export default Movies;
